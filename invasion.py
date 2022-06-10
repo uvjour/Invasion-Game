7fe2a879a1435ddb57bcbccc150c06de5d4b7b42
uvjour <uvjour@gmail.com>
2022-06-09 21:28:09 -0400

    update color

--- invasion.py
+++ invasion.py
@@ -2,16 +2,23 @@ import sys
 import pygame
 
 class Invasion():
+    # init function
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Invasion')
+
+        # Background Color
+        self.bg_color = (230, 230, 230)
 
     def rungame(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # Redrawing the color at every pass of the loop
+            self.screen.fill(self.bg_color)
             
             pygame.display.flip()
 
