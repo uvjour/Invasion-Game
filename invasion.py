d289b108f997fb857e2aa36d0c47595bef68d0a2
uvjour <uvjour@gmail.com>
2022-06-09 21:18:07 -0400

    update method

--- invasion.py
+++ invasion.py
@@ -6,3 +6,15 @@ class Invasion():
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Invasion')
+
+    def rungame(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ig = Invasion()
+    ig.rungame()
