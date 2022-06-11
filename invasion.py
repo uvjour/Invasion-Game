9fe5370ca8505295c7c426c21a9433c6a5b90ea3
uvjour <uvjour@gmail.com>
2022-06-11 19:20:57 -0400

    event break

--- invasion.py
+++ invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from settings import Settings
-
+from fighter import Fighter
 
 class Invasion():
     # init function
@@ -10,17 +10,34 @@ class Invasion():
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Invasion')
+        self.fighter = Fighter(self)
 
     def rungame(self):
         while True:
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+
+
+
+    def _check_events(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
-            # Redrawing the color at every pass of the loop
-            self.screen.fill(self.settings.bg_color)
-            
-            pygame.display.flip()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        # Move plane to the right
+                        self.fighter.rect.x += 1
+
+        
+
+    def _update_screen(self):
+        # Redrawing the color at every pass of the loop
+        self.screen.fill(self.settings.bg_color)
+        self.fighter.blitme()
+        
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     ig = Invasion()
