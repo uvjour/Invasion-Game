5c1433a3866e52598ea477db74b3fdb71d4079fe
uvjour <uvjour@gmail.com>
2022-06-12 17:38:01 -0400

    Add Bullets

---
+++ bullet.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    def __init__(self, ig_game):
+        super().__init__()
+        self.screen = ig_game.screen
+        self.settings = ig_game.settings
+        self.color = ig_game.settings.bullet_color
+        
+        self.rect = pygame.rect(0,0, ig_game.settings.bullet_width, ig_game.settings.bullet_height)
+        self.rect.midtop = ig_game.fighter.rect.midtop
+        self.y = float(self.rect.y)
+
+    def update(self):
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        
--- settings.py
+++ settings.py
@@ -8,4 +8,11 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.fighter_speed = 1.5
+        
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 1.5
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
 
