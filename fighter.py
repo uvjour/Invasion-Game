690604cac678265426f2ab1fab8f584135ce8bae
uvjour <uvjour@gmail.com>
2022-06-11 20:04:05 -0400

    update

--- fighter.py
+++ fighter.py
@@ -12,13 +12,14 @@ class Fighter():
         # movement flag
         self.moving_right = False
         self.moving_left = False
-
-    def blitme(self):
-        self.screen.blit(self.image, self.rect)
-
+    
     def update(self):
         if self.moving_right:
             self.rect.x += 1
         if self.moving_left:
             self.rect.x -= 1
+
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
 
