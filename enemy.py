779e2029f6673c8a69137c4256aa51cdf21c1c46
uvjour <uvjour@gmail.com>
2022-06-13 23:22:07 -0400

    update enemy

--- enemy.py
+++ enemy.py
@@ -1,3 +1,15 @@
 import pygame
 from pygame.sprite import Sprite
 
+class Enemy(Sprite):
+    def __init__(self, ig_game):
+        super().__init__()
+        self.screen = ig_game.screen
+
+        self.image = pygame.image.load('/images/enemy.bmp')
+        self.rect = self.image.get_rect()
+        
+        self.rect.x = self.rect.height
+        self.rect.y = self.rect.width
+
+        self.x = float(self.rect.x)
